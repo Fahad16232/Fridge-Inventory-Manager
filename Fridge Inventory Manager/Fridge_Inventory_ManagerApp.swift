90d10ac4e7a750235be1de34a478981b35372a8a
dsv <zxcv>
2024-10-02 19:40:10 +0500

    Initial Commit

---
+++ Fridge Inventory Manager/Fridge_Inventory_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fridge_Inventory_ManagerApp.swift
+//  Fridge Inventory Manager
+//
+//  Created by Muhammad Rizwan on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Fridge_Inventory_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
